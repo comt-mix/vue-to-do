4d1fffe2ac78797a073eabd095aac7d198f6695c
comt-mix <comt.mix@gmail.com>
2023-04-24 17:35:31 +0900

    Feat: components 분리 작업 진행 중

--- src/components/addList.vue
+++ src/components/addList.vue
@@ -1,10 +1,48 @@
 <template>
-  <div></div>
+      <li v-for="(todo, index) in {todoList}" :key="index">
+      <div class="box">
+        <div class="text">
+          <input v-model="todo.title" class="title" />
+          <input v-model="todo.content" class="content" />
+        </div>
+        <div class="icon">
+          <span @click="removeTodo(index)"
+            ><font-awesome-icon icon="fa-solid fa-trash"
+          /></span>
+          <span @click="updateTodo"
+            ><font-awesome-icon icon="fa-solid fa-pen-to-square"
+          /></span>
+        </div>
+        <button @click="completedTodo(todo.title, index)" :class="changeColor(isCompleted)">
+          {{ isCompleted ? "Completed" : "Pending" }}
+        </button>
+      </div>
+    </li>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      todoList: [{
+        title: "111",
+        content: "1",
+        isCompleted: false,
+      }, {
+        title: "222",
+        content: "2",
+        isCompleted: false,
+      }, {
+        title: "333",
+        content: "3",
+        isCompleted: false,
+      }],
+    };
+  },
   methods: {},
+  props: {
+    todoList: Array,
+  }
 };
 </script>
 
--- src/pages/MainVue.vue
+++ src/pages/MainVue.vue
@@ -9,38 +9,36 @@
         </button>
       </div>
     </div>
-
-    <li v-for="(todo, index) in todoList" :key="index">
-      <div class="box">
-        <div class="text">
-          <input v-model="todo.title" class="title" />
-          <input v-model="todo.content" class="content" />
-        </div>
-        <div class="icon">
-          <span @click="removeTodo(index)"
-            ><font-awesome-icon icon="fa-solid fa-trash"
-          /></span>
-          <span @click="updateTodo"
-            ><font-awesome-icon icon="fa-solid fa-pen-to-square"
-          /></span>
-        </div>
-        <button @click="completedTodo()" :class="changeColor(isCompleted)">
-          {{ isCompleted ? "Completed" : "Pending" }}
-        </button>
-      </div>
-    </li>
+    <AddList :todoList="todoList"></AddList>
   </div>
 </template>
 
 <script>
+import AddList from '@/components/AddList.vue';
+
 export default {
   data() {
     return {
       title: "",
       content: "",
-      todoList: [],
       isCompleted: false,
+      todoList: [{
+        title: "111",
+        content: "1",
+        isCompleted: false,
+      }, {
+        title: "222",
+        content: "2",
+        isCompleted: false,
+      }, {
+        title: "333",
+        content: "3",
+        isCompleted: false,
+      }],
     };
+  },
+  components: {
+    AddList
   },
   methods: {
     addTodo() {
@@ -57,9 +55,17 @@ export default {
     updateTodo() {
       console.log(this.todoList);
     },
-    completedTodo() {
-      this.isCompleted = !this.isCompleted;
-      console.log(this.todoList.isCompleted);
+    completedTodo(title, index) {
+
+      const list = this.todoList.map((list) => list.title)
+      
+      if (list.indexOf(title) === index) {
+        console.log(index, list.indexOf(title))
+        this.todoList[index].isCompleted = !this.todoList[index].isCompleted;
+        this.isCompleted = this.todoList[index].isCompleted;
+      }
+   
+      // console.log(this.todoList.isCompleted, this.isCompleted);
     },
     changeColor(isCompleted) {
       if (isCompleted) return "green";
